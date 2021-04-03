803df27f9ceb823ef455df1d5d63c7536e79d939
danellia <deshkadary2@gmail.com>
2021-04-03 15:24:05 +0700

    memento file editor finally works

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -7,10 +7,7 @@ namespace TextEditor
 {
     class Memento
     {
-        public string Name { get; set; }
-        public string Grade { get; set; }
-        public string Age { get; set; }
-        public string Interests { get; set; }
+        public string Text { get; set; }
     }
     public interface IOriginator
     {
@@ -18,69 +15,6 @@ namespace TextEditor
         void SetMemento(object memento);
     }
 
-    [Serializable]
-    class StudentInfo : IOriginator
-    {
-        public string Name { get; set; }
-        public string Grade { get; set; }
-        public string Age { get; set; }
-        public string Interests { get; set; }
-        public StudentInfo(string name, string grade, string age, string interests)
-        {
-            Name = name; Grade = grade; Age = age; Interests = interests;
-        }
-        object IOriginator.GetMemento()
-        {
-            return new Memento
-            {
-                Name = this.Name,
-                Grade = this.Grade,
-                Age = this.Age,
-                Interests = this.Interests
-            };
-        }
-        void IOriginator.SetMemento(object memento)
-        {
-            if (memento is Memento)
-            {
-                var mem = memento as Memento;
-                Name = mem.Name;
-                Grade = mem.Grade;
-                Age = mem.Age;
-                Interests = mem.Interests;
-            }
-        }
-        public void SerializeBinary(FileStream fs)
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            bf.Serialize(fs, this);
-            fs.Flush();
-            fs.Close();
-        }
-        public void DeserializeBinary(FileStream fs)
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            StudentInfo deserialized = (StudentInfo)bf.Deserialize(fs);
-            fs.Close();
-        }
-        public void SerializeXml(FileStream fs)
-        {
-            XmlSerializer xs = new XmlSerializer(this.GetType());
-            xs.Serialize(fs, this);
-            fs.Flush();
-            fs.Close();
-        }
-        public void DeserializeXml(FileStream fs)
-        {
-            XmlSerializer xs = new XmlSerializer(this.GetType());
-            StudentInfo deserialized = (StudentInfo)xs.Deserialize(fs);
-            fs.Close();
-        }
-        public void Print()
-        {
-            Console.WriteLine("name={0} grade={1} age={2} interests={3}", Name, Grade, Age, Interests);
-        }
-    }
     public class Caretaker
     {
         private object memento;
@@ -95,30 +29,118 @@ namespace TextEditor
         }
     }
 
+    [Serializable]
+    class TextFile : IOriginator
+    {
+        public string Text { get; set; }
+        public TextFile(string text)
+        {
+            Text = text;
+        }
+        object IOriginator.GetMemento()
+        {
+            return new Memento
+            {
+                Text = this.Text
+            };
+        }
+        void IOriginator.SetMemento(object memento)
+        {
+            if (memento is Memento)
+            {
+                var mem = memento as Memento;
+                Text = mem.Text;
+            }
+        }
+        public void SerializeBinary(FileStream fs)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, this);
+            fs.Flush();
+            fs.Close();
+        }
+        public void DeserializeBinary(FileStream fs)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            TextFile deserialized = (TextFile)bf.Deserialize(fs);
+            Text = deserialized.Text;
+            fs.Close();
+        }
+        public void SerializeXml(FileStream fs)
+        {
+            XmlSerializer xs = new XmlSerializer(this.GetType());
+            xs.Serialize(fs, this);
+            fs.Flush();
+            fs.Close();
+        }
+        public void DeserializeXml(FileStream fs)
+        {
+            XmlSerializer xs = new XmlSerializer(this.GetType());
+            TextFile deserialized = (TextFile)xs.Deserialize(fs);
+            Text = deserialized.Text;
+            fs.Close();
+        }
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.DarkGreen;
-            StudentInfo student = new StudentInfo("Will", "12", "18", "Music");
-            Console.WriteLine("creating a new file!...\nwrite \"stop\" on the new line to stop writing into the file");
 
-            Random random = new Random();
-            string filename = Convert.ToString(random.Next());
-            FileStream fs = new FileStream(filename + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+            Console.WriteLine("enter name of the file you want to create or open: ");
+            string filename = Console.ReadLine();
+            FileStream fs = new FileStream(filename + ".txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            Console.WriteLine(
+                "\nhere you go!" +
+                "\nnew line commands: save, undo, exit\n\n");
+
+            string input = "";
+            using (StreamReader sr = new StreamReader(filename + ".txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    input += line + "\n";
+                }
+            }
+
+            fs = new FileStream(filename + ".txt", FileMode.Append, FileAccess.Write);
             StreamWriter sw = new StreamWriter(fs);
-            string input;
+            Caretaker caretaker = new Caretaker();
+            
+            TextFile tf = new TextFile(input);
             while (true)
             {
                 input = Console.ReadLine();
-                if (input == "stop")
+
+                if (input == "save")
+                {
+                    caretaker.SaveState(tf);
+                    continue;
+                }
+                else if (input == "undo")
+                {
+                    caretaker.RestoreState(tf);
+                    fs = new FileStream(filename + ".txt", FileMode.Truncate, FileAccess.Write);
+                    sw = new StreamWriter(fs);
+                    sw.WriteLine(tf.Text);
+                    continue;
+                }
+                else if (input == "exit")
                 {
                     sw.Close();
                     break;
                 }
+                tf.Text += input + "\n";
                 sw.WriteLine(input);
             }
-
+        
         }
     }
 }
